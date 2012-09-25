d79b332766d827ef8b18c34867c9bf88cd263087
Florent Messa <florent.messa@gmail.com>
2012-09-25 14:40:29 +0200

    Add README content in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
+import os
 from setuptools import setup, find_packages
 
+root = os.path.abspath(os.path.dirname(__file__))
+
 version = __import__('events_watcher').__version__
+
+with open(os.path.join(root, 'README.rst')) as f:
+    README = f.read()
 
 setup(
     name='django-events-watcher',
     version=version,
     description='Events watcher is an event packaging library for Django to track changes made in your models.',
+    long_description=README,
     author='Florent Messa',
     author_email='florent.messa@gmail.com',
     url='http://github.com/thoas/django-events-watcher',
